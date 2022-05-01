453399b13c074cecd12a6d3be2ce82711b2cbeac
GSdaBlessedFist <gsdablessedfist@gmail.com>
2022-04-30 20:38:26 -0500

    Started work on haiku words...2)possible parchment paper bg

--- public/js/leafColorizing.js
+++ public/js/leafColorizing.js
@@ -4,6 +4,22 @@ const totalWidth = window.innerWidth;
 var color1 ="hsl(180,83%,47%)";//"hsl(45,93%,47%)"
 var color2 ="hsl(192, 83%, 47%)";//"hsl(0, 93%, 47%)"
 
+const haiku = document.querySelectorAll("[id^='haiku-']");
+
+function displayHaiku(){
+  let haikuTimeline = gsap.timeline({paused:true});  
+
+  haikuTimeline.to(haiku,{
+    opactiy: 1,
+    stagger:{
+      from:"start",
+      each: .25,
+      ease: "power2.inOut"
+    }
+  })
+}
+
+
 window.addEventListener("mousemove", (event) => {
   let x = event.pageX;
   var trunkControl = 35;//plus
@@ -15,6 +31,7 @@ window.addEventListener("mousemove", (event) => {
   const bonsaiDark = document.querySelector("#tree-darks");
   const trunkShape = document.querySelector("#trunk-shade-8");
   const bgHue = document.querySelector("#bonsai-background");
+  const haikuFrame = document.querySelector("#haikuFrame");
 
 
   let colors = gsap.utils.mapRange(0, totalWidth, 0, 1,x);  
@@ -50,8 +67,6 @@ window.addEventListener("mousemove", (event) => {
   trunkShape.style.mixBlendMode = "multiply";
   
   bgHue.style.background = `radial-gradient(circle, rgba(255,255,255,1) 0%, ${colorPick})`;
-
-
 })
 
 
